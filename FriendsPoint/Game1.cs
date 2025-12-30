e16d166aa3dee19a3750744343a9a04a8db45f28
arishem <abbasovaajnur07@gmail.com>
2025-12-31 02:12:23 +0500

    Commit 3
    - Загрузил нормальную(скорее всего временную, конечно) текстуру игрока в MGCB editor и также создал отдельные папки для спрайтов в MGCB
    - Сделал коллайдер для игрока
    - Чуть чуть переделал отрисовку, настроил центры объектов и масштабирование
    - Сделал вращение игрока по направлению курсора мыши

--- FriendsPoint/Game1.cs
+++ FriendsPoint/Game1.cs
@@ -28,33 +28,29 @@ namespace FriendsPoint
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            player = new Player(new Vector2(100, 100));
-
-            // создаем текстуру 1x1 для прямоугольника
+            player = new Player(new Vector2(100, 100)); // Стартовая позиция игрока
             pixel = new Texture2D(GraphicsDevice, 1, 1);
-            pixel.SetData(new[] { Color.White });
-            player.Texture = pixel; // назначаем текстуру игроку
+            pixel.SetData(new[] { Color.Black });
+            player.Pixel = pixel;
+            player.Texture = Content.Load<Texture2D>("players/soldierRF"); // Назначаем текстуру игроку
         }
 
         protected override void Update(GameTime gameTime)
         {
+            KeyboardState state = Keyboard.GetState();
+            MouseState mouse = Mouse.GetState();
+            player.direction(mouse.Position);
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
             // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
-        private int timer = 0;
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.White);
-            timer++;
             _spriteBatch.Begin();
-
-            // рисуем игрока
             player.Draw(_spriteBatch);
-
             _spriteBatch.End();
             base.Draw(gameTime);
         }
--- FriendsPoint/Player.cs
+++ FriendsPoint/Player.cs
@@ -1,20 +1,54 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
 
 public class Player {
-    public Vector2 Position;       // позиция игрока
-    public int Width = 50;         // ширина прямоугольника
-    public int Height = 80;        // высота прямоугольника
-    public Texture2D Texture;      // текстура игрока
-
+    public Vector2 Position;    // Позиция игрока
+    public int Width = 170;     // Ширина прямоугольника
+    public int Height = 170;    // Высота прямоугольника
+    public Texture2D Texture;   // Текстура игрока
+    public Texture2D Pixel; // Пиксель
+    public float Rotation = 0;
     public Player(Vector2 startPosition) {
         Position = startPosition;
+    }
+
+    public void direction(Point mousePosition)
+    {
+        Vector2 mousedirection = new Vector2(mousePosition.X - Position.X, mousePosition.Y - Position.Y);
+        Rotation = (float)Math.Atan2(mousedirection.Y, mousedirection.X) + MathHelper.PiOver2;
     }
 
     // Метод для отрисовки игрока
     public void Draw(SpriteBatch spriteBatch) {
         if (Texture != null) {
-            spriteBatch.Draw(Texture, new Rectangle((int)Position.X, (int)Position.Y, Width, Height), Color.Black);
+            Rectangle Rect = new Rectangle((int)Position.X, (int)Position.Y, Width, Height);
+
+            spriteBatch.Draw(
+                Texture, 
+                Position, // Положение 
+                null, // Прямоугольник
+                Color.White, // Цвет
+                Rotation, // Вращение
+                new Vector2(Texture.Width * 0.5f, Texture.Height * 0.5f), // Центр объекта, вокруг которого происходит вращение и тд.
+                0.35f, // Масштабирование
+                SpriteEffects.None, // Отражение по горизонтали и вертикали
+                0.0f // Глубина
+                );
+
+            spriteBatch.Draw(
+                Pixel, 
+                Position, 
+                Rect, 
+                Color.Black * 0.5f, 
+                0.0f,
+                new Vector2(Height, Width) * 0.5f,
+                0.35f,  
+                SpriteEffects.None,
+                0.0f);
         }
     }
+
 }
